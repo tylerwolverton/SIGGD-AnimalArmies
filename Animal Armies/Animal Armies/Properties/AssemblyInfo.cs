77e718fd3339608098ae0b3f30817904da2b5136
tylerwolverton <tylerwolvert@gmail.com>
2018-10-07 11:29:29 -0400

    Added installer project and required dlls

--- Animal Armies/Animal Armies/Properties/AssemblyInfo.cs
+++ Animal Armies/Animal Armies/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Game")]
+[assembly: AssemblyTitle("Animal Armies")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Purdue University Computer Science Department")]
